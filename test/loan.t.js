c3b8358377944d9dd39fbcf1ab144caa50514f2b
Nadav Hollander <nadavhollander@Nadavs-MacBook-Pro.local>
2017-06-23 18:01:00 -0500

    repaymentys working

--- test/loan.t.js
+++ test/loan.t.js
@@ -133,5 +133,53 @@ describe('Loan', function() {
         else done();
       })
     })
+
+    it("should transfer the balance to a user when the loan's fully funded", function(done) {
+      const amount = 200;
+      const funder = web3.eth.accounts[2];
+      const borrowerBalanceBefore = web3.eth.getBalance(web3.eth.accounts[0]);
+      loan.fund(amount, funder, { from: funder }, function(err, txHash) {
+        if (err) done(err);
+        const borrowerBalanceAfter = web3.eth.getBalance(web3.eth.accounts[0]);
+        expect(borrowerBalanceAfter.sub(borrowerBalanceBefore).equals(terms.principal)).to.be(true);
+        done();
+      })
+    })
+  })
+
+  describe("#repay()", function(done) {
+    let uuid = web3.sha3(uuidV4());
+    let loan = new Loan(web3, uuid, terms);
+
+    before(function(done) {
+      loan.broadcast(function(err, txHash) {
+        if (err) done(err)
+        loan.attest('QmaF1vXQDHnn5MVgfRc54Hs1ivemMDdfLhZABpuJwQwuPE',
+          { from: web3.eth.accounts[1], gas: 500000 }, function(err, result) {
+          if (err) done(err);
+          else done();
+        })
+      })
+    })
+
+    it("should not let a user make a repayment before the loan is fully funded", function(done) {
+      loan.repay(100, { from: web3.eth.accounts[0] }, function(err, result) {
+        if (!err) done("should return error");
+        else done();
+      })
+    })
+
+    it("should let a user make a repayment once the loan's fully funded", function(done) {
+      loan.fund(1000, web3.eth.accounts[3], { from: web3.eth.accounts[3] }, function(err, result) {
+        if (err) done(err)
+        loan.repay(100, { from: web3.eth.accounts[0] }, function(err, result) {
+          if (err) done(err)
+          else {
+            expect(loan.getRedeemableValue().equals(100)).to.be(true);
+            done();
+          }
+        })
+      })
+    })
   })
 })
