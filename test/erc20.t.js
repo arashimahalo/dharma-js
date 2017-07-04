99e66ae2e98c0a14ae1533fade8055b702cf0ae8
Nadav Hollander <nadavhollander@Nadavs-MacBook-Pro.local>
2017-07-04 16:28:11 -0700

    erc20 tests passing

--- test/erc20.t.js
+++ test/erc20.t.js
@@ -1,47 +1,28 @@
-const web3 = require('./init.js');
-const Loan = require('../lib/loan.js');
-const Metadata = require('../package.json');
-const expect = require('expect.js');
-const uuidV4 = require('uuid/v4');
+import {web3, util} from './init.js';
+import Loan from '../lib/loan.js';
+import Metadata from '../package.json';
+import expect from 'expect.js';
+import uuidV4 from 'uuid/v4';
+import TestLoans from './util/TestLoans';
+import {Transfer, Approval} from './util/LoanEvents';
 
 describe('ERC20', function() {
   let loan;
 
   before(async function() {
-    loan = await new Promise(function(accept, reject) {
-      web3.eth.getBlock('latest', function(err, result) {
-        if (err) reject(err);
-        else {
-          const timelock = result.timestamp + 60 * 60;
-          terms = {
-            borrower: ACCOUNTS[0],
-            attestor: ACCOUNTS[1],
-            principal: 1000,
-            interest: 5,
-            periodType: 'daily',
-            periodLength: 1,
-            termLength: 3,
-            fundingPeriodTimeLock: timelock
-          };
-          uuid = web3.sha3(uuidV4());
-          accept(new Loan(web3, uuid, terms));
-        }
-      })
-    });
-
-    await loan.broadcast();
-    await loan.attest('QmaF1vXQDHnn5MVgfRc54Hs1ivemMDdfLhZABpuJwQwuPE',
-      { from: ACCOUNTS[1], gas: 500000 });
-    await loan.fund(1000, ACCOUNTS[2], { from: ACCOUNTS[2] });
+    loan = await TestLoans.LoanInAcceptedState(ACCOUNTS);
   })
 
   describe('#transfer()', function() {
     it("should allow an investor to transfer their balance to someone else", async function() {
-      await loan.transfer(ACCOUNTS[3], 400, { from: ACCOUNTS[2] });
-      const toBalance = await loan.balanceOf(ACCOUNTS[3]);
-      expect(toBalance.equals(400)).to.be(true);
+      const amount = web3.toWei(0.1, 'ether');
+
+      const fromBalanceBefore = await loan.balanceOf(ACCOUNTS[2]);
+      await loan.transfer(ACCOUNTS[7], amount, { from: ACCOUNTS[2] });
+      const toBalance = await loan.balanceOf(ACCOUNTS[7]);
+      expect(toBalance.equals(amount)).to.be(true);
       const fromBalance = await loan.balanceOf(ACCOUNTS[2]);
-      expect(fromBalance.equals(600)).to.be(true);
+      expect(fromBalance.equals(fromBalanceBefore.minus(amount))).to.be(true);
     })
 
     it("should prevent a non-inveestor from transfering anyone's balance", async function() {
@@ -64,9 +45,10 @@ describe('ERC20', function() {
 
   describe('#transferFrom()', function() {
     it("should allow someone with an approved balance to transfer on an owner's behalf", async function() {
+      const balanceBefore = await loan.balanceOf(ACCOUNTS[5]);
       await loan.transferFrom(ACCOUNTS[2], ACCOUNTS[5], 100, { from: ACCOUNTS[4] });
-      const balance = await loan.balanceOf(ACCOUNTS[5]);
-      expect(balance.equals(100)).to.be(true);
+      const balanceAfter =  await loan.balanceOf(ACCOUNTS[5]);
+      expect(balanceAfter.minus(balanceBefore).equals(100)).to.be(true);
     })
 
     it("should not allow someone with an unapproved balance to transfer on an owner's behalf", async function() {
@@ -80,44 +62,42 @@ describe('ERC20', function() {
   })
 
   describe('#events', function() {
-    it('should callback on Transfer event', async function() {
-      const value = 300;
-      return new Promise(async function(accept, reject) {
-        const transferEvent = await loan.events.transfer();
-        transferEvent.watch(function(err, obj) {
-          if (err) reject(err);
-          else {
-            expect(obj.args._uuid).to.be(uuid)
-            expect(obj.args._from).to.be(ACCOUNTS[2])
-            expect(obj.args._to).to.be(ACCOUNTS[3])
-            expect(obj.args._value.equals(value)).to.be(true)
-            transferEvent.stopWatching();
-            accept();
-          }
-        })
+    it('should callback on Transfer event', async () => {
+      const amount = web3.toWei(0.03, 'ether');
 
-        await loan.transfer(ACCOUNTS[3], value, { from: ACCOUNTS[2] });
+      const blockNumber = await util.getLatestBlockNumber(web3);
+      const transferEvent = await loan.events.transfer();
+      transferEvent.watch(function(err, result) {
+        util.assertEventEquality(result, Transfer({
+          uuid: loan.uuid,
+          from: ACCOUNTS[3],
+          to: ACCOUNTS[5],
+          value: amount,
+          blockNumber: blockNumber + 1
+        }))
+        transferEvent.stopWatching();
       })
+
+      await loan.transfer(ACCOUNTS[5], amount, { from: ACCOUNTS[3] });
     })
 
     it('should callback on Approval event', async function() {
-      const value = 300;
-      return new Promise(async function(accept, reject) {
-        const approvalEvent = await loan.events.approval();
-        approvalEvent.watch(function(err, obj) {
-          if (err) reject(err);
-          else {
-            expect(obj.args._uuid).to.be(uuid)
-            expect(obj.args._owner).to.be(ACCOUNTS[2])
-            expect(obj.args._spender).to.be(ACCOUNTS[5])
-            expect(obj.args._value.equals(value)).to.be(true)
-            approvalEvent.stopWatching();
-            accept();
-          }
-        })
+      const amount = web3.toWei(0.03, 'ether');
 
-        await loan.approve(ACCOUNTS[5], value, { from: ACCOUNTS[2] });
-      });
-    })
+      const blockNumber = await util.getLatestBlockNumber(web3);
+      const approvalEvent = await loan.events.approval();
+      approvalEvent.watch(function(err, result) {
+        util.assertEventEquality(result, Transfer({
+          uuid: loan.uuid,
+          from: ACCOUNTS[5],
+          to: ACCOUNTS[3],
+          value: amount,
+          blockNumber: blockNumber + 1
+        }))
+        approvalEvent.stopWatching();
+      })
+
+      await loan.approve(ACCOUNTS[3], amount, { from: ACCOUNTS[5] });
+    });
   })
 })
