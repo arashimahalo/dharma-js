ab0924904d94f57b917c2f675757bb27faa37584
Nadav Hollander <nadavhollander@Nadavs-MacBook-Pro.local>
2017-07-03 17:24:09 -0700

    added schemas -- untested

--- test/attestations/example.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "defaultLikelihood": 0.3,
-  "uuid": "0xc97f0442e704837a9b7f37be02959b87a1c1c04107b49e900cd30862543ef8a7",
-  "r": "0xdc00638a7c953bba7a7b7b6fd7a390fb84c7ad7d1dbe4fec15ba77c7b7fccf",
-  "s": "cb67d3410563493bf337092b7e37837351e86f90c0b7a54fcc668a003b6df04f8200",
-  "v": "8200"
-}
