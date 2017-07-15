8d556b397e47938a84f4c8507a1611025c7b8678
Nadav Hollander <nadavhollander@nadavs-mbp.attlocal.net>
2017-07-14 21:42:48 -0700

    getRepaymentStatus added to servicer

--- src/Servicing.js
+++ src/Servicing.js
@@ -26,17 +26,67 @@ var Servicing = function () {
     value: function expectedAmountRepaidByDate(date) {
       if (!this.loan.termBeginTimestamp) throw new Error('Loan must be in ACCEPTED state and refreshState() must' + ' be called before servicing utilities can be accessed');
 
+      var expectedPeriodicRepayment = this.periodicRepaymentOwed();
+
+      var termBeginDate = new Date(this.loan.termBeginTimestamp * 1000);
+      var numPeriods = this._numPeriodsBetween(termBeginDate, date, this.loan.terms.periodType(), this.loan.terms.periodLength());
+      return expectedPeriodicRepayment.times(numPeriods);
+    }
+  }, {
+    key: 'getRepaymentStatus',
+    value: async function getRepaymentStatus() {
+      var amountRepaid = await this.loan.amountRepaid();
+      var expectedAmountRepaid = this.expectedAmountRepaidByDate(new Date());
+
+      if (amountRepaid.gte(expectedAmountRepaid)) {
+        return 'REPAID';
+      } else {
+        var numPeriodsRepaid = amountRepaid.div(this.periodicRepaymentOwed()).floor().toNumber();
+        var lastRepaymentDateMissed = (0, _moment2.default)(this.loan.termBeginTimestamp * 1000).add(this.periodDuration(numPeriodsRepaid + 1));
+        var weeksSinceRepaymentDateMissed = _moment2.default.duration((0, _moment2.default)().diff(lastRepaymentDateMissed)).asWeeks();
+
+        if (weeksSinceRepaymentDateMissed <= 2) {
+          return 'DELINQUENT';
+        } else {
+          return 'DEFAULT';
+        }
+      }
+    }
+  }, {
+    key: 'periodicRepaymentOwed',
+    value: function periodicRepaymentOwed() {
+      return this.totalOwed().div(this.loan.terms.termLength());
+    }
+  }, {
+    key: 'totalOwed',
+    value: function totalOwed() {
       var decimals = 10 ** 18;
 
       var interestRate = this.loan.interestRate;
       interestRate = interestRate.div(decimals).plus(1);
 
       var principal = this.loan.principal;
-      var expectedPeriodicRepayment = interestRate.times(principal).div(this.loan.terms.termLength());
+      return interestRate.times(principal);
+    }
+  }, {
+    key: 'periodDuration',
+    value: function periodDuration() {
+      var numPeriods = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
 
-      var termBeginDate = new Date(this.loan.termBeginTimestamp * 1000);
-      var numPeriods = this._numPeriodsBetween(termBeginDate, date, this.loan.terms.periodType(), this.loan.terms.periodLength());
-      return expectedPeriodicRepayment.times(numPeriods);
+      switch (this.loan.terms.periodType()) {
+        case 'daily':
+          return _moment2.default.duration(numPeriods * this.loan.terms.periodLength(), 'days');
+          break;
+        case 'weekly':
+          return _moment2.default.duration(numPeriods * this.loan.terms.periodLength(), 'weeks');
+          break;
+        case 'monthly':
+          return _moment2.default.duration(numPeriods * this.loan.terms.periodLength(), 'months');
+          break;
+        case 'yearly':
+          return _moment2.default.duration(numPeriods * this.loan.terms.periodLength(), 'years');
+          break;
+      }
     }
   }, {
     key: '_numPeriodsBetween',
--- test/servicing.t.js
+++ test/servicing.t.js
@@ -1,6 +1,9 @@
 import {LoanInAcceptedState} from './util/TestLoans';
 import {web3, util} from './init';
 import expect from 'expect.js';
+import MockDate from 'mockdate';
+import moment from 'moment';
+
 
 describe('Servicing', () => {
   describe('#expectedAmountRepaidByDate()', () => {
@@ -207,6 +210,103 @@ describe('Servicing', () => {
         const returnedExpectedAmountRepaid = loan.servicing.expectedAmountRepaidByDate(date)
         expect(returnedExpectedAmountRepaid
           .equals(expectedPeriodicRepayment.times(2))).to.be(true);
+      })
+    });
+  })
+
+  describe('#getRepaymentStatus', () => {
+    let loan;
+    let date;
+    let amountOwed;
+    let periodicRepaymentOwed;
+
+    beforeEach(async () => {
+      loan = await LoanInAcceptedState(ACCOUNTS, {
+        terms: {
+          version: 1,
+          periodType: 'weekly',
+          periodLength: 1,
+          termLength: 2,
+          compounded: false
+        }
+      })
+
+      await loan.refreshState()
+
+      date = new Date();
+
+      const decimals = web3.toWei(1, 'ether');
+      let interestRate = await loan.getInterestRate();
+      interestRate = interestRate.div(decimals).plus(1);
+
+      const principal = loan.principal;
+
+      amountOwed = interestRate.times(principal);
+      periodicRepaymentOwed = amountOwed.div(2);
+    })
+
+    afterEach(MockDate.reset);
+
+    describe('repaid in full', () => {
+      beforeEach(async () => {
+        const date = moment().add(15, 'days').toDate();
+        MockDate.set(date);
+        await loan.repay(amountOwed);
+      })
+
+      it ('should classify the loan as REPAID', async () => {
+        const status = await loan.servicing.getRepaymentStatus();
+        expect(status).to.be('REPAID');
+      })
+    });
+
+    describe('delinquent in part', () => {
+      beforeEach(async () => {
+        const date = moment().add(20, 'days').toDate();
+        MockDate.set(date);
+        await loan.repay(amountOwed.times(0.6));
+      })
+
+      it ('should classify the loan as DELINQUENT', async () => {
+        const status = await loan.servicing.getRepaymentStatus();
+        expect(status).to.be('DELINQUENT');
+      })
+    });
+
+    describe('delinquent in full', () => {
+      beforeEach(async () => {
+        const date = moment().add(20, 'days').toDate();
+        MockDate.set(date);
+      })
+
+      it ('should classify the loan as DELINQUENT', async () => {
+        const status = await loan.servicing.getRepaymentStatus();
+        expect(status).to.be('DELINQUENT');
+      })
+    });
+
+    describe('defaulted in part', () => {
+      beforeEach(async () => {
+        const date = moment().add(30, 'days').toDate();
+        MockDate.set(date);
+        await loan.repay(amountOwed.times(0.6));
+      })
+
+      it ('should classify the loan as DEFAULT', async () => {
+        const status = await loan.servicing.getRepaymentStatus();
+        expect(status).to.be('DEFAULT');
+      })
+    });
+
+    describe('defaulted in full', () => {
+      beforeEach(async () => {
+        const date = moment().add(30, 'days').toDate();
+        MockDate.set(date);
+      })
+
+      it ('should classify the loan as DEFAULT', async () => {
+        const status = await loan.servicing.getRepaymentStatus();
+        expect(status).to.be('DEFAULT');
       })
     });
   })
