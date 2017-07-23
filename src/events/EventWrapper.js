3e83bb888af53f6d737e7ede035aeef26a117535
Nadav Hollander <nadavhollander@nadavs-mbp.attlocal.net>
2017-07-22 18:44:29 -0700

    fix

--- src/events/EventWrapper.js
+++ src/events/EventWrapper.js
@@ -2,9 +2,9 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _V = require('uuid/V4');
+var _uuid = require('uuid');
 
-var _V2 = _interopRequireDefault(_V);
+var _uuid2 = _interopRequireDefault(_uuid);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -15,7 +15,7 @@ var EventWrapper = function () {
     _classCallCheck(this, EventWrapper);
 
     this.event = event;
-    this.id = (0, _V2.default)();
+    this.id = (0, _uuid2.default)();
     this.queue = queue;
 
     if (callback) {
