d96cdbeb53ce784856f864cfbbdffef36139345b
Nadav Hollander <nadavhollander@nadavs-mbp.attlocal.net>
2017-07-22 18:06:16 -0700

    fixing require paths

--- src/Dharma.js
+++ src/Dharma.js
@@ -1,15 +1,22 @@
 'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var _loans = require('./loans');
 
-var Loans = require('./loans');
-var Metadata = require('../package.json');
+var _loans2 = _interopRequireDefault(_loans);
+
+var _package = require('../package.json');
+
+var _package2 = _interopRequireDefault(_package);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Dharma = function Dharma(web3) {
   _classCallCheck(this, Dharma);
 
   this.web3 = web3;
-  this.loans = new Loans(web3);
+  this.loans = new _loans2.default(web3);
 };
 
 module.exports = Dharma;
