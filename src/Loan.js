2fb8fa4b3d219b9df4f01aaa3336d8e152b12d65
Nadav Hollander <nadavhollander@nadavs-mbp.attlocal.net>
2017-07-23 15:24:59 -0700

    updated default gas allowance

--- src/Loan.js
+++ src/Loan.js
@@ -68,7 +68,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var UNDEFINED_GAS_ALLOWANCE = 1000000;
+var UNDEFINED_GAS_ALLOWANCE = 500000;
 
 var Loan = function (_RedeemableERC) {
   _inherits(Loan, _RedeemableERC);
